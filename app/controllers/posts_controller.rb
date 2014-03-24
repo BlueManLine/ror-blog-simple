f5877b10fcaf930af3a61e6ba6300c6802889009
Szymon Bluma <szymon.bluma@polcode.net>
2014-03-24 10:38:53 +0100

    destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    
+    # GET /posts
     def index
+        @posts = Post.all
     end
 
     # GET /posts/new
@@ -22,11 +26,24 @@ class PostsController < ApplicationController
         end
     end
 
+    # DELETE /post/1
+    def destroy
+        @post.destroy
+        respond_to do |format|
+          format.html { redirect_to posts_url }
+          #format.json { head :no_content }
+        end
+    end
 
 
     private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :post)
-    end
+        # Use callbacks to share common setup or constraints between actions.
+        def set_post
+          @post = Post.find(params[:id])
+        end
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def post_params
+          params.require(:post).permit(:title, :post)
+        end
 end
