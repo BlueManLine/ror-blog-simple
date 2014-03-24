927046322631d2c413d822dcde47740d8b643336
Szymon Bluma <szymon.bluma@polcode.net>
2014-03-24 10:27:07 +0100

    sluged title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    before_save :make_slug
+
+    private
+
+    def make_slug
+        self.slug = self.title.downcase.gsub(/[^a-z1-9]+/, '-').chomp('-')
+    end
 end
